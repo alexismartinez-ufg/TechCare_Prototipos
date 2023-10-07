efb74748d7348f31902ee748da66ec00c5669f5b
AlexisMartínez <ia.alexismartinez@ufg.edu.sv>
2023-10-07 17:54:15 -0600

    Agregar vistas y controladores de comedor y reserva

---
+++ TechCare_Prototipos/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechCare_Prototipos.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index() => RedirectToAction("Login");
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ TechCare_Prototipos/Controllers/ReservaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechCare_Prototipos.Controllers
+{
+    public class ReservaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Exitosa()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
